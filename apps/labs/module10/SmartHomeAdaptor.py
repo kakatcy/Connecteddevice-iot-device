25b7d4e437820e33b101058d80cb8db26ff69ead
cytang <cytang@10.0.0.60>
2019-12-03 11:34:29 -0500

    final

--- apps/labs/module10/SmartHomeAdaptor.py
+++ apps/labs/module10/SmartHomeAdaptor.py
@@ -5,12 +5,13 @@ Created on Dec 1, 2019
 '''
 from labs.module10.AutoTemperature import AutoTemperature
 from labs.module10.AutoWatering import AutoWatering
+from time import sleep
 
 class SmartHomeAdaptor():
     
     def __init__(self):
         autoTemperature = AutoTemperature()
         autoTemperature.client_pub()
-        
+        sleep(1)
         autoWatering = AutoWatering()
         autoWatering.client_pub()
--- apps/labs/module10/SmartHomeApp.py
+++ apps/labs/module10/SmartHomeApp.py
@@ -4,6 +4,9 @@ Created on Dec 1, 2019
 @author: cytang
 '''
 from labs.module10.SmartHomeAdaptor import SmartHomeAdaptor
+import logging
 
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.DEBUG)
+logging.info("Starting smart home system performance app daemon thread...")
 
 smartHomeAdaptor = SmartHomeAdaptor()
