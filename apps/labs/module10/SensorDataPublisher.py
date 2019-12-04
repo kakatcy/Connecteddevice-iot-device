b4a7e4ee19e19aa5a495a69700d165b8e3150886
cytang <cytang@10.0.0.60>
2019-12-03 22:14:38 -0500

    final

---
+++ apps/labs/module10/SensorDataPublisher.py
@@ -0,0 +1,68 @@
+'''
+Created on Dec 1, 2019
+
+@author: cytang
+'''
+import paho.mqtt.client as mqtt
+import logging
+from time import sleep
+from labs.module06.MqttClientConnector import MqttClientConnector
+from labs.module10.UbidotsConnector import UbidotsConnector
+from sense_hat import SenseHat
+
+rootCertPath = "/Users/cytang/program/connected devices/ubidots_cert.pem"
+topicTemp = "/v1.6/devices/finaldevice/Temperature"
+topicHumidity = "/v1.6/devices/finaldevice/Humidity" 
+
+#collect the temperature data
+class SensorDataPublisher:
+    json = None
+    def client_pub(self):
+        sense_hat = SenseHat()
+        #ubidots
+        ubidotsConnector = UbidotsConnector()
+        ubidotsConnector.connect()
+        
+        #MQTT
+        client = mqtt.Client("pub_py")
+        mqttClientConnector = MqttClientConnector(client,True,rootCertPath)
+        #set the callback methods
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+        client.on_disconnect = self.on_disconnect
+        #connect to the broker
+        mqttClientConnector.connect(client)
+        while True:
+            #get temperature 
+            temperature = sense_hat.get_temperature_from_humidity()
+            logging.info("current temperature:" + str(temperature))
+            
+            #publish the temperature to ubidots by using ubidots API
+            #ubidotsConnector.publishTemp(topicTemp, 0, temperature)
+            
+            #publish the current temperature to ubidots by mqtt
+            client.publish(topicTemp, temperature, 1, True) 
+            logging.info("published temperature successfully")
+            
+            #get humidity 
+            humidity = sense_hat.get_humidity()
+            logging.info("current humidity:" + str(humidity))
+            ubidotsConnector.publishHumidity(topicHumidity, 0, humidity)
+            logging.info("published humidity successfully")
+            sleep(60) 
+
+    def on_connect(self, client, userdata, flags, rc):
+        logging.info("Connected with result code "+str(rc))
+        client.subscribe("test",qos=2)
+    
+    def on_disconnect(self, client, userdata, rc):
+        logging.info("disconnecting reason "+ str(rc))
+
+    def on_message(self, client, userdata, msg):
+        #log the jsondata that the subsriber received
+        logging.info("the second json:\n" +str(msg.payload.decode("utf-8")))    
+        logging.info("message topic="+str(msg.topic))
+        logging.info("message qos="+str(msg.qos))
+        logging.info("message retain flag="+str(msg.retain))    
+        self.json = str(msg.payload.decode("utf-8"))
+        #logging.info("the second json:\n" + self.json)
--- apps/labs/module10/SmartHomeAdaptor.py
+++ apps/labs/module10/SmartHomeAdaptor.py
@@ -3,17 +3,10 @@ Created on Dec 1, 2019
 
 @author: cytang
 '''
-from labs.module10.AutoTemperature import AutoTemperature
-from labs.module10.AutoWatering import AutoWatering
-from time import sleep
+from labs.module10.SensorDataPublisher import SensorDataPublisher
 
 class SmartHomeAdaptor():
     
     def __init__(self):
-        autoTemperature = AutoTemperature()
-        autoTemperature.client_pub()
-        '''
-        sleep(1)
-        autoWatering = AutoWatering()
-        autoWatering.client_pub()
-        '''
+        sensorDataPublisher = SensorDataPublisher()
+        sensorDataPublisher.client_pub()
