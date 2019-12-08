49d3e7417c53386ed2e508c726cb918ad74b8fe2
cytang <cytang@10.0.0.60>
2019-12-08 11:41:39 -0500

    final

--- apps/project/SourceHandler.py
+++ apps/project/SourceHandler.py
@@ -9,7 +9,14 @@ class SourceHandler(Resource):
     def __init__(self, name="ResourceHandler", coap_server=None):
         super(SourceHandler, self).__init__(name, coap_server, visible=True,
                                             observable=True, allow_children=True)
-    def handle_POST(self, request):
+    def render_POST(self, request):
+        Resource.render_POST(self, request)
         payload = request.payload
+        print("111")
         print(payload)
         return payload
+    
+    def render_GET(self, request):
+        Resource.render_GET(self, request)
+        print(request)
+        return self
