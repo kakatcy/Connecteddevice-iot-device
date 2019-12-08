19114d4d94981aa45507912b0e21fbe2fe9cdae6
cytang <cytang@10.0.0.60>
2019-12-08 10:40:32 -0500

    final

--- apps/project/SourceHandler.py
+++ apps/project/SourceHandler.py
@@ -6,6 +6,9 @@ Created on Dec 8, 2019
 from coapthon.resources.resource import Resource
 
 class SourceHandler(Resource):
+    def __init__(self, name="ResourceHandler", coap_server=None):
+        super(SourceHandler, self).__init__(name, coap_server, visible=True,
+                                            observable=True, allow_children=True)
     def handle_POST(self, request):
         payload = request.payload
         print(payload)
