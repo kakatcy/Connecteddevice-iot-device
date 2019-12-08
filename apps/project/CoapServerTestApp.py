bb0835b0a92bb172bd0e55d163208a683aeb6b36
cytang <cytang@10.0.0.60>
2019-12-08 12:08:38 -0500

    final

--- apps/project/CoapServerTestApp.py
+++ apps/project/CoapServerTestApp.py
@@ -1,21 +1,21 @@
-'''
-Created on Dec 8, 2019
-
-@author: cytang
-'''
 from coapthon.server.coap import CoAP
-from project.SourceHandler import SourceHandler
+from project import BasicResource
 
 class CoAPServer(CoAP):
     def __init__(self, host, port):
         CoAP.__init__(self, (host, port))
-        self.add_resource('temp1/',SourceHandler())
-        
+        self.add_resource('temp1/', BasicResource())
+
 def main():
-    server = CoAPServer("10.0.0.175", 5683)
-    print("server is running")
-    #try:
-        #server.listen(10)
+    server = CoAPServer("0.0.0.0", 5683)
+    try:
+        server.listen(10)
+    except KeyboardInterrupt:
+        #print "Server Shutdown"
+        print("Server Shutdown")
+        server.close()
+        print("Exiting...")
+        #print "Exiting..."
         
 if __name__ == '__main__':
     main()
