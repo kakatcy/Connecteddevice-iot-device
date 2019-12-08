b1f0c8ab1e30748aa7716cdea2e9256ceb68fa5e
cytang <cytang@10.0.0.60>
2019-12-08 12:11:02 -0500

    final

--- apps/project/BasicResource.py
+++ apps/project/BasicResource.py
@@ -5,18 +5,24 @@ Created on Dec 8, 2019
 '''
 from coapthon.resources.resource import Resource
 
-class SourceHandler(Resource):
-    def __init__(self, name="ResourceHandler", coap_server=None):
-        super(SourceHandler, self).__init__(name, coap_server, visible=True,
+class BasicResource(Resource):
+    def __init__(self, name="BasicResource", coap_server=None):
+        super(BasicResource, self).__init__(name, coap_server, visible=True,
                                             observable=True, allow_children=True)
-    def render_POST(self, request):
-        Resource.render_POST(self, request)
-        payload = request.payload
-        print("111")
-        print(payload)
-        return payload
-    
+        self.payload = "Basic Resource"
+
     def render_GET(self, request):
-        Resource.render_GET(self, request)
-        print(request)
         return self
+
+    def render_PUT(self, request):
+        self.payload = request.payload
+        return self
+
+    def render_POST(self, request):
+        res = BasicResource()
+        res.location_query = request.uri_query
+        res.payload = request.payload
+        return res
+
+    def render_DELETE(self, request):
+        return True
